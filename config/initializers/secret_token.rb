de494fddfea6ff8eb4e76eaf3272cb3fa34153d2
Jane Sternbach <janesternbach@gmail.com>
2013-08-31 17:18:45 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SauceOff::Application.config.secret_token = 'b02709783605eace9c6f4d54107b1db15ef83f501b8bffc87131769d109f8172da4d6029bfd43ca5638166dd5053443a51e54cdd64fc52cbd87a19fbac6be523'
